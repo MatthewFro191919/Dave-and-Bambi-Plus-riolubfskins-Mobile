b4a90b264e2c211d13a90276d6688df4a0d7960a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-10 09:44:12 -0500

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,23 +4,21 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp.git
-haxelib git lime https://github.com/mcagabe19-stuff/lime
-haxelib install openfl 9.2.2
-haxelib install flixel 5.2.2
-haxelib install flixel-tools 1.5.1
-haxelib install flixel-ui 2.5.0
-haxelib install flixel-addons 2.10.0
-haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-haxelib git polymod https://github.com/larsiusprime/polymod.git
-haxelib install hxcpp-debug-server
-haxelib install actuate
-haxelib install hxCodec 2.5.1
-haxelib install hscript
-haxelib git lime https://github.com/mcagabe19-stuff/lime
-haxelib git discord_rpc https://github.com/YoshiCrafter29/linc_discord-rpc
+haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
+haxelib git lime https://github.com/mcagabe19-stuff/lime --quiet
+haxelib install openfl 9.3.3 --quiet
+haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
+haxelib install flixel-addons 3.2.2 --quiet
+haxelib install flixel-tools 1.5.1 --quiet
+haxelib install flixel-ui --quiet
+haxelib install hscript-iris 1.1.3 --quiet
+haxelib install tjson 1.4.0 --quiet
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
+haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
+haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
+haxelib install hxvlc 1.9.2 --quiet --skip-dependencies
+haxelib install hxCodec 2.5.1 --quiet --skip-dependencies
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 echo Finished!
 pause
